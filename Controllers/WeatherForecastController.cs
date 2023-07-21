3c1682f5781f695b009d2c38be4550c62423b6f2
Ioan-Paul Niculae <niculae.paul@gmail.com>
2023-07-21 12:46:29 +0300

    added serilog

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.Log(LogLevel.Information, "Test Log");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+    
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
